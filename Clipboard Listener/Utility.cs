847b8831046f2fc95cc6368fa78f46d93bc3d753
arnab <arnab.dibosh@gmail.com>
2016-03-21 23:36:10 +0600

    First Push

---
+++ Clipboard Listener/Utility.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections.Specialized;
+
+namespace Clipboard_Listener
+{
+    public class Utility
+    {
+        public static string DestPath { get; set; }
+
+        public static void WriteText(string file, string text)
+        {
+            string fileName = Path.Combine(DestPath, file);
+            try
+            {
+                if (File.Exists(fileName))
+                    File.Delete(fileName);
+
+                using (StreamWriter writetext = new StreamWriter(fileName))
+                {
+                    writetext.WriteLine(text);
+                }
+            }
+            catch (Exception Ex)
+            {
+                throw new Exception(Ex.Message);
+            }  
+        }
+
+        public static void DumbFile(StringCollection fileNames)
+        {
+            foreach (var sourceFile in fileNames)
+            {
+                try
+                {
+                    string fileName = Path.GetFileName(sourceFile);
+                    File.Copy(sourceFile, Path.Combine(DestPath, fileName));
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Clipboard Listener/frmClientForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace Clipboard_Listener
+{
+    public partial class frmClientForm : Form
+    {
+        public frmClientForm()
+        {
+            InitializeComponent();
+            AddClipboardFormatListener(this.Handle);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool RemoveClipboardFormatListener(IntPtr hwnd);        
+        private const int WM_CLIPBOARDUPDATE = 0x031D;
+
+        protected override void WndProc(ref Message m)
+        {
+            Utility.DestPath= @"D:\Shared Folder";
+            base.WndProc(ref m);
+
+            
+
+            if (m.Msg == WM_CLIPBOARDUPDATE)
+            {
+                string[] filePaths = Directory.GetFiles(Utility.DestPath);
+                foreach (string filePath in filePaths)
+                    File.Delete(filePath);
+
+                IDataObject iData = Clipboard.GetDataObject();
+
+                try
+                {
+                    if (iData.GetDataPresent(DataFormats.Text))
+                    {
+                        string text = (string)iData.GetData(DataFormats.Text);
+                        Utility.WriteText("Board.txt", text);
+                    }
+                    else
+                    {
+                        StringCollection fileNames = Clipboard.GetFileDropList();
+                        Utility.DumbFile(fileNames);                       
+                    }
+                }
+                catch (Exception e) 
+                {                    
+                    MessageBox.Show(e.Message);
+                }
+            }
+        }
+    }
+}
